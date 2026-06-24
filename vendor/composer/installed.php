dada8bdd78d70beeea63aa63850e5912f700e364
borhan-m-uddin <borhanuddin1902@gmail.com>
2026-06-24 17:20:42 +0600

    Add vendor folder for deployment (forced)

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'borhan-m-uddin/sharehub',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '1a618bf5c8affcf4063825c5f42ac7383a91a5e8',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'borhan-m-uddin/sharehub' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '1a618bf5c8affcf4063825c5f42ac7383a91a5e8',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v6.10.0',
+            'version' => '6.10.0.0',
+            'reference' => 'bf74d75a1fde6beaa34a0ddae2ec5fce0f72a144',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
